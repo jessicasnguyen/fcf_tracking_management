44e9591d6fc42d2b4f088632b117c4d9d4c4f12a
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-08-05 13:21:38 -0600

    customers controller done

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -1,2 +1,43 @@
 class Api::CustomersController < ApplicationController
+    before_action :set_customer, only: [:update, :show, :destroy]
+
+    def index
+        render json: Customer.all
+    end
+
+    def show
+        render json: @customer
+    end
+
+    def create
+        customer = Customer.new(customer_params)
+        if customer.save
+            render json: customer
+        else
+            render json: {errors: @customer.errors.full_messages}, status: 422
+        end
+    end
+
+    def update
+        if(@customer.update(customer_params))
+            render json: @customer
+        else
+            render json: {errors: @customer.errors.full_message}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @customer.destroy
+    end
+
+    private
+
+    def set_customer
+        @customer = Customer.find(params[:id])
+    end
+
+    def customer_params
+        params.require(:customer).permit(:first_name, :last_name, :email, :company, :custtomer_cat, :phone, :mobile, :fax, :website, :street, :city, :state, :zip, :country)
+    end
+
 end
