4bac39f110c0e132f4e7ec2af3be14f4f13758ca
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-08-16 09:26:31 -0600

    customer show started

--- client/src/invoice/pages/CustomerShow.js
+++ client/src/invoice/pages/CustomerShow.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import { useParams, useLocation, useNavigate, Link } from 'react-router-dom'
+import { DataContext } from '../../providers/DataProvider'
+import CustomerForm from '../forms/CustomerForm'
 
 const CustomerShow = () => {
-  return <div>CustomerShow</div>
+  const { updateCustomer, deleteCustomer } = useContext(DataContext)
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [customer, setCustomer] = useState([])
+  const [showForm, setShowForm] = useState(false)
+  const [showEditForm, setShowEditForm] = useState(false)
+  const params = useParams()
+
+  useEffect(() => {
+    getCustomer()
+  }, [])
+
+  const addCusForm = () => {
+    let path = `/customers/new`
+    navigate(path)
+  }
+
+  const editCusForm = () => {
+    let path = `/customers/${customer.id}/edit`
+    navigate(path)
+  }
+
+  const getCustomer = async () => {
+    try {
+      let res = await axios.get(`/api/customers/${params.id}`)
+      setCustomer(res.data)
+    } catch (err) {
+      alert('Error in getting customer')
+    }
+  }
+
+  return (
+    <div>
+      <h1>
+        {customer.first_name} {customer.last_name}
+      </h1>
+    </div>
+  )
 }
 
 export default CustomerShow
